bc979e61be94c211fdd207ce304b74061f01c726
jzolo22 <jzolotarev@gmail.com>
2020-11-14 21:14:36 -0500

    commit tree 4834836e4bf5fc38e16a7cb4f575b39f232b875a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++)
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    return newArray
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--)
+        console.log(i);
+}
